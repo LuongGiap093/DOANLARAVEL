d31af57096bb37f99291cc0701cd03354a32dd0f
dinhtrong201000 <67587090+dinhtrong201000@users.noreply.github.com>
2021-05-30 12:47:25 +0700

    cap nhat 30/5/2021

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::group(['prefix' => 'panel', 'namespace' => 'admin'], function() {	
+Route::group(['prefix' => 'panel', 'namespace' => 'admin'], function() {
 	Route::get('login','LoginController@getLogin')->name('getLogin');
 	Route::post('login','LoginController@postLogin')->name('postLogin');
 	Route::get('logout','LoginController@getLogout')->name('getLogout');
@@ -27,19 +27,10 @@ Route::resource('panel/product',admin\ProductController::class);
 Route::resource('panel/category',admin\CategoryController::class);
 Route::resource('panel/customer',admin\CustomerController::class);
 Route::resource('panel/order',admin\OrderController::class);
-/* Route::resource('panel/news',admin\CategoryNews::class); */
-
 Route::get('panel/category/productlist/{id}','admin\CategoryController@productlist')->name('category.productlist');
 
 
 Route::group(['prefix' => 'product', 'namespace' => 'FrontEnd'], function() {
-/* 	Route::get('/', 'ProductsController@index');
-	Route::get('cart', 'ProductsController@cart');
-	Route::get('add-to-cart/{id}', 'ProductsController@addToCart');
-	Route::patch('update-cart', 'ProductsController@update');
-	Route::delete('remove-from-cart', 'ProductsController@remove'); */
-
-	
 	Route::get('checkout','ProductsController@DetailsCheckout');
 	Route::get('hoanthanh','ProductsController@hoanthanh');
 	Route::post('dathang','ProductsController@postCheckout')->name('dathang');
@@ -48,29 +39,16 @@ Route::group(['prefix' => 'product', 'namespace' => 'FrontEnd'], function() {
 
 Route::group(['prefix' => '', 'namespace' => 'user'], function() {
 		Route::get('', 'UserController@index')->name('shopping.index');
-		Route::get('category', 'UserController@category')->name('shopping.category');	
-		//Route::get('product', 'UserController@product')->name('shopping.product');
-		//Route::get('cartdetail', 'UserController@cartDetail')->name('shopping.cartdetail');
+		Route::get('category', 'UserController@category')->name('shopping.category');
 		Route::get('viewCart', 'UserController@viewCart')->name('shopping.viewCart');
+		Route::get('blog','UserController@blog')->name('shopping.blog');
 		Route::get('details/{id}','UserController@viewProduct')->name('shopping.viewProduct');
-		//Route::get('checkout','UserController@getCheckout')->name('shopping.getCheckout');
-		
 		Route::get('addCart/{id}','UserController@AddCart')->name('shopping.addCart');
 		Route::get('details/addCart/{id}','UserController@AddCart')->name('shopping.addCart');
 		Route::get('deleteItemCart/{id}','UserController@deleteItemCart')->name('shopping.deleteItemCart');
 		Route::get('details/deleteItemCart/{id}','UserController@deleteItemCart'); //Dung de delete Cart trang details
-		
 		Route::get('delete-ListItemCart/{id}','UserController@deleteListItemCart')->name('shopping.delete-ListItemCart');
 		Route::get('save-ListItemCart/{id}/{quanty}','UserController@saveListItemCart')->name('shopping.save-ListItemCart');
-
 		Route::get('loaisp/{id}','UserController@getsp')->name('shopping.loaisp');
 });
-/*
-GET	    /product	        		index	product.index
-GET	    /product/create	    		create	product.create
-POST	/product					store	product.store
-GET		/product/{product}			show	product.show
-GET		/product/{product}/edit		edit	product.edit
-PUT/PATCH	/product/{product}		update	product.update
-DELETE	/ product/{product}			destroy	product.destroy
-*/
+
