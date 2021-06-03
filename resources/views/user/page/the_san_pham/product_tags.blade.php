e1ab32b3696594effd223d01aae95a8bed8d506f
dinhtrong201000 <67587090+dinhtrong201000@users.noreply.github.com>
2021-06-04 00:00:35 +0700

    update 3/6/2021

---
+++ resources/views/user/page/the_san_pham/product_tags.blade.php
@@ -0,0 +1,14 @@
+<div class="sidebar-widget product-tag wow fadeInUp">
+    <h3 class="section-title">Thẻ sản phẩm</h3>
+    <div class="sidebar-widget-body outer-top-xs">
+
+        <div class="tag-list">
+            @foreach($products as $product)
+                <a class="item" title="Phone" href="#">{{ $product->name }}</a>
+            @endforeach
+        </div>
+        <!-- /.tag-list -->
+    </div>
+    <!-- /.sidebar-widget-body -->
+</div>
+<!-- /.sidebar-widget -->
