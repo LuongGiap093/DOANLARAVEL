85cea2e3172c8fe17f646fca33db46c6292439c4
Luong <luong12cb2@gmail.com>
2021-10-28 22:21:17 +0700

    Update dashboard

--- resources/views/admin/dashboard/layout.blade.php
+++ resources/views/admin/dashboard/layout.blade.php
@@ -1,5 +1,48 @@
-<!DOCTYPE html>
+{{--<!DOCTYPE html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="utf-8">--}}
+{{--    <meta http-equiv="X-UA-Compatible" content="IE=edge">--}}
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">--}}
+{{--    <meta name="description" content="">--}}
+{{--    <meta name="author" content="">--}}
+{{--    <title>SB Admin 2 - Dashboard</title>--}}
+{{--    <link href="{!! asset('public\admin/vendor/fontawesome-free/css/all.min.css') !!}" rel="stylesheet" type="text/css">--}}
+{{--    <link--}}
+{{--        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"--}}
+{{--        rel="stylesheet">--}}
+{{--    <link href="{!! asset('public\admin/css/sb-admin-2.min.css') !!}" rel="stylesheet">--}}
+{{--</head>--}}
+{{--<body id="page-top">--}}
+{{--    <div id="wrapper">--}}
+{{--        @include('admin.theme.sidebar')	--}}
+{{--        <div id="content-wrapper" class="d-flex flex-column">--}}
+{{--            <div id="content">--}}
+{{--                @include('admin.theme.nav')--}}
+{{--                @if(Session::has('message'))--}}
+{{--                <div class="alert alert-success">--}}
+{{--                  {{ Session::get('message') }}--}}
+{{--                </div>--}}
+{{--                @endif	                   --}}
+{{--        @yield('content')--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--	</div>--}}
+{{--    --}}
+{{--  </body>--}}
+{{--</html>--}}
+{{--<script src="{!! asset('public\admin/vendor/jquery/jquery.min.js') !!}"></script>--}}
+{{--  <script src="{!! asset('public\admin/vendor/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>--}}
+{{--  <script src="{!! asset('public\admin/vendor/jquery-easing/jquery.easing.min.js') !!}"></script>--}}
+{{--  <script src="{!! asset('public\admin/js/sb-admin-2.min.js') !!}"></script>--}}
+{{--  <script src="{!! asset('public\admin/vendor/chart.js/Chart.min.js') !!}"></script>--}}
+{{--  <script src="{!! asset('public\admin/js/demo/chart-area-demo.js') !!}"></script>--}}
+{{--  <script src="{!! asset('public\admin/js/demo/chart-pie-demo.js') !!}"></script>--}}
+{{--    --}}
+
+        <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <title>Data Table | Velonic - Responsive Bootstrap 4 Admin Dashboard</title>
@@ -8,23 +51,18 @@
     <meta content="Coderthemes" name="author">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{!! asset('admin/assets/images/favicon.ico') !!}">
+    <link rel="shortcut icon" href="{!! asset('public\admin/assets/images/favicon.ico') !!}">
 
     <!-- third party css -->
-    <link href="{!! asset('admin/assets/libs/datatables/dataTables.bootstrap4.min.css') !!}" rel="stylesheet"
-          type="text/css">
-    <link href="{!! asset('admin/assets/libs/datatables/buttons.bootstrap4.min.css') !!}" rel="stylesheet"
-          type="text/css">
-    <link href="{!! asset('admin/assets/libs/datatables/responsive.bootstrap4.min.css') !!}" rel="stylesheet"
-          type="text/css">
-    <link href="{!! asset('admin/assets/libs/datatables/select.bootstrap4.min.css') !!}" rel="stylesheet"
-          type="text/css">
+    <link href="{!! asset('public\admin/assets/libs/datatables/dataTables.bootstrap4.min.css') !!}" rel="stylesheet" type="text/css">
+    <link href="{!! asset('public\admin/assets/libs/datatables/buttons.bootstrap4.min.css') !!}" rel="stylesheet" type="text/css">
+    <link href="{!! asset('public\admin/assets/libs/datatables/responsive.bootstrap4.min.css') !!}" rel="stylesheet" type="text/css">
+    <link href="{!! asset('public\admin/assets/libs/datatables/select.bootstrap4.min.css') !!}" rel="stylesheet" type="text/css">
 
     <!-- App css -->
-    <link href="{!! asset('admin/assets/css/bootstrap.min.css') !!}" rel="stylesheet" type="text/css"
-          id="bootstrap-stylesheet">
-    <link href="{!! asset('admin/assets/css/icons.min.css') !!}" rel="stylesheet" type="text/css">
-    <link href="{!! asset('admin/assets/css/app.min.css') !!}" rel="stylesheet" type="text/css" id="app-stylesheet">
+    <link href="{!! asset('public\admin/assets/css/bootstrap.min.css') !!}" rel="stylesheet" type="text/css" id="bootstrap-stylesheet">
+    <link href="{!! asset('public\admin/assets/css/icons.min.css') !!}" rel="stylesheet" type="text/css">
+    <link href="{!! asset('public\admin/assets/css/app.min.css') !!}" rel="stylesheet" type="text/css" id="app-stylesheet">
     <script src="//cdn.ckeditor.com/4.16.1/full/ckeditor.js"></script>
 
 </head>
@@ -43,12 +81,12 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box">
-                            <h4 class="page-title">Welcome !</h4>
+                            <h4 class="page-title">Data Table</h4>
                             <div class="page-title-right">
                                 <ol class="breadcrumb p-0 m-0">
                                     <li class="breadcrumb-item"><a href="#">Velonic</a></li>
-                                    <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                                    <li class="breadcrumb-item active">Dashboard 1</li>
+                                    <li class="breadcrumb-item"><a href="#">Tables</a></li>
+                                    <li class="breadcrumb-item active">Data Table</li>
                                 </ol>
                             </div>
                             <div class="clearfix"></div>
@@ -57,9 +95,16 @@
                 </div>
 
 
-                @yield('content')
-
-
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body table-responsive">
+                                <h4 class="m-t-0 header-title mb-4"><b>Doanh Thu Sản Phẩm</b></h4>
+                                @yield('content')
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -97,8 +142,7 @@
                 <strong>Customize </strong> the overall color scheme, layout, etc.
             </div>
             <div class="mb-2">
-                <img src="{!! asset('admin/assets/images/layouts/light.png') !!}" class="img-fluid img-thumbnail"
-                     alt="">
+                <img src="{!! asset('public\admin/assets/images/layouts/light.png') !!}" class="img-fluid img-thumbnail" alt="">
             </div>
             <div class="custom-control custom-switch mb-3">
                 <input type="checkbox" class="custom-control-input theme-choice" id="light-mode-switch" checked="">
@@ -106,12 +150,10 @@
             </div>
 
             <div class="mb-2">
-                <img src="{!! asset('admin/assets/images/layouts/dark.png') !!}" class="img-fluid img-thumbnail" alt="">
+                <img src="{!! asset('public\admin/assets/images/layouts/dark.png') !!}" class="img-fluid img-thumbnail" alt="">
             </div>
             <div class="custom-control custom-switch mb-3">
-                <input type="checkbox" class="custom-control-input theme-choice" id="dark-mode-switch"
-                       data-bsstyle="{!!asset(' admin/assets/css/bootstrap-dark.min.css') !!}"
-                       data-appstyle="{!! asset('admin/assets/css/app-dark.min.css') !!}">
+                <input type="checkbox" class="custom-control-input theme-choice" id="dark-mode-switch" data-bsstyle="{!!asset(' admin/assets/css/bootstrap-dark.min.css') !!}" data-appstyle="{!! asset('public\admin/assets/css/app-dark.min.css') !!}">
                 <label class="custom-control-label" for="dark-mode-switch">Dark Mode</label>
             </div>
 
@@ -128,34 +170,32 @@
 </a>
 
 <!-- Vendor js -->
-<script src="{!! asset('admin/assets/js/vendor.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/js/vendor.min.js') !!}"></script>
 
 <!-- Required datatable js -->
-<script src="{!! asset('admin/assets/libs/datatables/jquery.dataTables.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/datatables/dataTables.bootstrap4.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/jquery.dataTables.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/dataTables.bootstrap4.min.js') !!}"></script>
 <!-- Buttons examples -->
-<script src="{!! asset('admin/assets/libs/datatables/dataTables.buttons.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/datatables/buttons.bootstrap4.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/jszip/jszip.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/pdfmake/pdfmake.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/pdfmake/vfs_fonts.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/datatables/buttons.html5.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/datatables/buttons.print.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/dataTables.buttons.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/buttons.bootstrap4.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/jszip/jszip.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/pdfmake/pdfmake.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/pdfmake/vfs_fonts.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/buttons.html5.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/buttons.print.min.js') !!}"></script>
 
 <!-- Responsive examples -->
-<script src="{!! asset('admin/assets/libs/datatables/dataTables.responsive.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/datatables/responsive.bootstrap4.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/dataTables.responsive.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/responsive.bootstrap4.min.js') !!}"></script>
 
-<script src="{!! asset('admin/assets/libs/datatables/dataTables.keyTable.min.js') !!}"></script>
-<script src="{!! asset('admin/assets/libs/datatables/dataTables.select.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/dataTables.keyTable.min.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/libs/datatables/dataTables.select.min.js') !!}"></script>
 
 <!-- Datatables init -->
-<script src="{!! asset('admin/assets/js/pages/datatables.init.js') !!}"></script>
+<script src="{!! asset('public\admin/assets/js/pages/datatables.init.js') !!}"></script>
 
 <!-- App js -->
-<script src="{!! asset('admin/assets/js/app.min.js') !!}"></script>
-{{--<script src="{!! asset('admin/assets/js/dashboard3.js') !!}"></script>--}}
-
+<script src="{!! asset('public\admin/assets/js/app.min.js') !!}"></script>
 <script>CKEDITOR.replace('contents')</script>
 <script>CKEDITOR.replace('data')</script>
 
@@ -163,15 +203,12 @@
 <script>
     $(function () {
         'use strict'
-
         var ticksStyle = {
             fontColor: '#495057',
             fontStyle: 'bold'
         }
-
         var mode = 'index'
         var intersect = true
-
         var $visitorsChart = $('#visitors-chart')
         // eslint-disable-next-line no-unused-vars
         var visitorsChart = new Chart($visitorsChart, {
